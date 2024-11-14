48e0b57dc5587be40ed2f24d7ce120210bfc68d0
Belal <bb5503344@gmail.com>
2024-11-14 23:44:45 +0200

    oo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./Components/1-Navbar/Navbar";
 import Swiper from './Components/2-HomePage/SwiperC'
 import SliderCategory  from './Components/3-Category/SliderCategory'
+import Product from "./Components/4-Products/Product";
 // eslint-disable-next-line no-unused-vars
 import HeaderNav from "./Components/1-Navbar/HeaderNav";
 import "./App.css";
 // eslint-disable-next-line no-unused-vars
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { cyan } from "@mui/material/colors";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const theme = createTheme({
@@ -33,6 +34,7 @@ function App() {
           <Navbar />
           <Swiper />
           <SliderCategory />
+          <Product />
         </ThemeProvider>
       </BrowserRouter>
     </>
---
+++ src/Components/4-Products/Product.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import ProductCom from './ProductCom'
+
+export default function Product() {
+  return (
+    <div style={{marginTop:"100px"}}>
+        <ProductCom />
+      
+    </div>
+  )
+}
