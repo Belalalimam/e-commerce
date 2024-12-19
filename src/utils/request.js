5c5c1e982de2bab5ccedfee4dc005b47771d0f80
Belal <bb5503344@gmail.com>
2024-12-19 22:22:12 +0200

    oo

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
+  // baseURL: "https://royal-tex.shutterfly-alu.com",
   baseURL: "https://myserver-app.up.railway.app",
   // baseURL: "http://localhost:4000",
   headers: {
